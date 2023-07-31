65544f51feabbf57b43a79ab7f6b5e6ee5c861e8
Jackson Ventura <jacksonvbarbosa@terra.com.br>
2023-07-31 02:23:05 -0300

    Atualizando curso

---
+++ curso_javascript_1/conversoes.js
@@ -0,0 +1,9 @@
+//Tipos de dados
+//Booleanos
+
+//Convençao Implicita permite converter um tipo de dado em outro exemplo: número em string
+const numero1 = 456;
+const numeroString = "456" ;
+
+console.log(numero1 === numeroString);
+//Convençao Explicita
---
+++ curso_javascript_1/truthy e falsy.js
@@ -0,0 +1,26 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+//thrufy or false
+// 0 = false e 1 = true
+
+console.log(0 == false); // == - compara somente o valor
+console.log("" == false); // == - compara somente o valor
+
+
+//Null e Undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
